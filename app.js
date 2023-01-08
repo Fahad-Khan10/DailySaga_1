c796562f67c8c2d376fa29339d9d58bb8299aad5
Fahad Khan <78487993+Fahad-Khan10@users.noreply.github.com>
2023-01-08 23:46:22 +0530

    First

--- app.js
+++ app.js
@@ -21,11 +21,11 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 const homeContent =
-  "Whether sharing your expertise, breaking news, or whatever’s on your mind, you’re in good company on our website Daily Journal. Begin a journey to discover why millions of people have published their passions here.";
+  "Whether sharing your expertise, breaking news, or whatever’s on your mind, you’re in good company on our website MJ Saga. Begin a journey to discover why millions of people have published their passions here.";
 const aboutContent =
-  "Since 1999, millions of people have expressed themselves on our website Daily Journal. From detailed posts about almost every apple variety you could ever imagine to a blog dedicated to the art of blogging itself, the ability to easily share, publish and express oneself on the web is at the core of Blogger’s mission. As the web constantly evolves, we want to ensure anyone using Daily Journal has an easy and intuitive experience publishing their content to the web.";
+  "Since 1999, millions of people have expressed themselves on our website MJ Saga. From detailed posts about almost every apple variety you could ever imagine to a blog dedicated to the art of blogging itself, the ability to easily share, publish and express oneself on the web is at the core of Blogger’s mission. As the web constantly evolves, we want to ensure anyone using MJ Saga has an easy and intuitive experience publishing their content to the web.";
 const contactContent =
-  "It looks like you’re trying to reach Daily Saga’s customer service team. To make your life a little easier, please get in contact with Daily Saga’s representatives by reaching out to them directly using the contact :: E-Mail Id : DailySaga@gmail.com";
+  "It looks like you’re trying to reach MJSaga’s customer service team. To make your life a little easier, please get in contact with MJSaga’s representatives by reaching out to them directly using the contact :: E-Mail Id : MJSaga@gmail.com";
 
 const monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
@@ -46,6 +46,7 @@ const composeSchema = new mongoose.Schema({
 
 
 const composeItem = mongoose.model("composeItem",composeSchema);
+const hotItem = mongoose.model("hotItem",composeSchema);
 
 
 
@@ -60,7 +61,26 @@ const composeItem = mongoose.model("composeItem",composeSchema);
 
 app.get("/", function (req, res) {
 
-    res.sendFile(__dirname + "/dailysaga.html");
+    res.sendFile(__dirname + "/MJSaga.html");
+
+});
+
+
+/// Hot Topics
+
+
+app.get("/hot", function (req, res) {
+
+  //returns array
+  hotItem.find({},function(err,resultArray){
+
+    res.render("hot", {
+      homeContent_ejs: homeContent,
+      posts: resultArray,
+      //sending result array to home for iterting through loop to get title and text
+    });
+
+  });
 
 });
 
@@ -183,8 +203,6 @@ app.post("/home", function (req, res) {
       res.redirect("/home");
     }
   });
-  
-    
 
 });
 
